41aa6d65e3e42777b66c7a42c5ff5f98297a128c
Mahdi <mahdimey17@gmail.com>
2023-11-27 01:01:16 +0330

    added hour & minute functionailty

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 const mainColor = window.getComputedStyle(document.documentElement).getPropertyValue('--main-color')
 const fadeColor = window.getComputedStyle(document.documentElement).getPropertyValue('--fade-color')
 
-const hour   = document.querySelector('.hour .main')
-const minute = document.querySelector('.minute .main')
+const hourElement   = document.querySelector('.hour .main')
+const minuteElement = document.querySelector('.minute .main')
 
 const am = document.querySelector('.amORpm .am')
 const pm = document.querySelector('.amORpm .pm')
+
+const data = new Date()
+
+let hour   = data.getHours()
+let minute = data.getMinutes()
+
+hourElement.innerHTML = hour
+minuteElement.innerHTML = minute
