1473398fc1d6ca0b6e1f835a28e4d2e6781d86d4
Mahdi <mahdimey17@gmail.com>
2023-11-27 00:56:55 +0330

    selected some variables

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+// color variables from css
 const mainColor = window.getComputedStyle(document.documentElement).getPropertyValue('--main-color')
 const fadeColor = window.getComputedStyle(document.documentElement).getPropertyValue('--fade-color')
 
-console.log(mainColor, fadeColor)
+const hour   = document.querySelector('.hour .main')
+const minute = document.querySelector('.minute .main')
+
+const am = document.querySelector('.amORpm .am')
+const pm = document.querySelector('.amORpm .pm')
