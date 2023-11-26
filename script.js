f7ce5657678e3dd0ddb657bf3375ceddc44f4652
Mahdi <mahdimey17@gmail.com>
2023-11-26 22:32:28 +0330

    selected css color vars in js

---
+++ script.js
@@ -0,0 +1,4 @@
+const mainColor = window.getComputedStyle(document.documentElement).getPropertyValue('--main-color')
+const fadeColor = window.getComputedStyle(document.documentElement).getPropertyValue('--fade-color')
+
+console.log(mainColor, fadeColor)
